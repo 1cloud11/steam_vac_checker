01607d8e4d4f8d92370461e995a72f76bd4338bd
Kot Vladislav <kot.vladislav@ukr.net>
2020-12-03 00:31:18 +0200

    дописал запрос на json за кол-ом дней с последнего vac-бана

--- steam_id_finder.py
+++ steam_id_finder.py
@@ -1,9 +1,10 @@
 import requests
-import xml.etree.ElementTree as et 
+import xml.etree.ElementTree as et
+from settings import STEAM_API_KEY
 
-user_data = 'https://steamcommunity.com/id/alex_pimenov/'
+user_url = 'https://steamcommunity.com/id/alex_pimenov/'
 
-def get_steamid(user_data):
+def get_userinfo(user_data):
     user_url_xml = user_data + '?xml=1'
     root = et.fromstring(requests.get(user_url_xml).content)
         
@@ -14,14 +15,35 @@ def get_steamid(user_data):
     print(f'Ник пользователя на момент репорта {steam_name}')
     print(f'Количество VAC-банов: {vac}')
 
+    json_url = 'http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=' + STEAM_API_KEY + '&steamids=' + steamid64
+    params = {
+        'format': 'json',
+    }
+
+    result = requests.get(json_url, params=params)
+    json_data = result.json()
+    #След конструкция возвращает False, нужно посмотреть что не так
+    if 'players' in json_data:
+        if 'DaysSinceLastBan' in json_data['players']:
+            try:
+                last_vac = json_data['players']['DaysSinceLastBan']
+                print(f'С последнего VAC-бана прошло {last_vac}')
+            except(IndexError, TypeError):
+                return False
+    return False
+
+if __name__ == "__main__":
+    g = get_userinfo(user_url)
+    print(g)
+
     #Создать список словарей, в который будет вносится новый словарь в случае добавления юзера.
     #Словарь в виде {'steamid': steamid, 'steam_name': steam_name, {'VAC': True, 'VAC_count': int, 'last_VAC': timedelta, }
     #Перед внесением юзера программа должна проверить по steamid, нет ли уже такого юзера в списке. Если есть - программа должна получить новые данные по VAC, и сообщить, были ли изменения.
     #Если изменений нет - отразить старую информацию, если есть - обозначить, что информация новая (дописать New), и указать новую.
     #
 
-get_steamid(user_data)
-#def vac_checker(steam_id):
+
+
 
 
 
